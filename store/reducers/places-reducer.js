86fbed5682fcbaf94b934454d555533fcd013918
Pravin <pgangad0@gmail.com>
2020-08-06 22:59:16 +0530

    #feature-11- fetch data from local db

--- store/reducers/places-reducer.js
+++ store/reducers/places-reducer.js
@@ -1,5 +1,5 @@
 import { acc } from "react-native-reanimated";
-import { ADD_PLACE } from "../actions/places-actions";
+import { ADD_PLACE, SET_PLACES } from "../actions/places-actions";
 import Place from "../../models/place";
 const initialState = {
   places: [],
@@ -7,6 +7,12 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case SET_PLACES:
+      return {
+        places: action.places.map(
+          (pl) => new Place(pl.id.toString(), pl.title, pl.imageUri)
+        ),
+      };
     case ADD_PLACE:
       const newPlace = new Place(
         action.placeData.id.toString(),
