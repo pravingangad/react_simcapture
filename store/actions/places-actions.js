0924a99bbef9e94712708bc17274e65870888dff
Pravin <pgangad0@gmail.com>
2020-08-06 17:38:16 +0530

    file system add

--- store/actions/places-actions.js
+++ store/actions/places-actions.js
@@ -1,5 +1,23 @@
+import * as FileSystem from "expo-file-system";
+
 export const ADD_PLACE = "ADD_PLACE";
 
-export const addPlace = (title,image) => {
-  return { type: ADD_PLACE, placeData: { title: title,image:image } };
+export const addPlace = (title, image) => {
+  return async (dispatch) => {
+    const fileName = image.split("/").pop();
+    const newPath = FileSystem.documentDirectory + fileName;
+    console.log(newPath);
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath,
+      });
+  
+    }catch(error){
+      console.log(error);
+      throw(error);
+    }
+    
+    dispatch({ type: ADD_PLACE, placeData: { title: title, image: image } });
+  };
 };
